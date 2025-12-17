987a1aea6a86b35c012820350a1ac3f8a3121a53
kangyh04 <kangyh04@gmail.com>
2025-12-17 13:02:11 +0900

    scene fade controller and view

--- R/Assets/Scripts/UI/Controller/FadeController.cs
+++ R/Assets/Scripts/UI/Controller/FadeController.cs
@@ -2,15 +2,33 @@ using UnityEngine;
 
 public class FadeController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField]
+    private FadeView fadeView;
+
+    [SerializeField]
+    private float fadeTime;
+
+    public async void FadeInAsync()
     {
-        
+        await fadeView.Fade(0.0f, fadeTime, fadeTime);
     }
 
-    // Update is called once per frame
-    void Update()
+    public async void FadeOutAsync()
     {
-        
+        await fadeView.Fade(fadeTime, 0.0f, fadeTime);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Debug.LogError("Fade In");
+            FadeInAsync();
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Debug.LogError("Fade Out");
+            FadeOutAsync();
+        }
     }
 }
